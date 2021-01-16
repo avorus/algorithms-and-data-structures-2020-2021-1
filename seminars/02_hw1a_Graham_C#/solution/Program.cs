7415cf23c4770b0cc42e6707a7b8e55fd63247b7
Churt's <64949882+Crutchs@users.noreply.github.com>
2021-01-16 16:13:38 +0300

    add seminars part 1

---
+++ seminars/02_hw1a_Graham_C#/solution/Program.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Graham
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Чтение файла
+            List<Point> points = ReadFile(args[2]);
+            if (points != null)
+            {
+                //Осуществление прохода по точкам
+                List<Point> mbo = GrahamAlgorithm(points);
+                if (mbo != null)
+                {
+                    //Добавляем первую точку в конец стека для удобных преобразований при выводе без потери первой точки
+                    mbo.Add(mbo[0]);
+                    //Если нужен проход по часовой стрелке, то переворачиваем последовательность
+                    if (args[0] == "cw")
+                    {
+                        mbo.Reverse();
+                    }
+                    //Выбор соответствующего формата записи данных в файл
+                    if (args[1] == "plain")
+                    {
+                        mbo.RemoveAt(mbo.Count - 1);
+                        WriteFilePlain(args[3], mbo);
+                    }
+                    else
+                    {
+                        WriteFileWKT(args[3], points, mbo);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Осуществляет чтение файла. Если возникла ошибка, то возвращает null
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static List<Point> ReadFile(string path)
+        {
+            try
+            {
+                string[] input = File.ReadAllLines(path);
+                int count = int.Parse(input[0]);
+                List<Point> points = new List<Point>();
+                for (int i = 1; i <= count; ++i)
+                {
+                    string[] point = input[i].Split(' ');
+                    int x = int.Parse(point[0]);
+                    int y = int.Parse(point[1]);
+                    points.Add(new Point(x, y));
+                }
+                return points;
+            }
+            catch
+            {
+                Console.WriteLine("Oops... Something wrong with reading the file.");
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Осуществляет поиск минимальной выпуклой оболочки с помощью прохода Грэхема на основе стека
+        /// Проход осуществляется против часовой стрелки
+        /// </summary>
+        /// <param name="input">исходная последовательность точек</param>
+        /// <returns>последовательность точек, которые образуют минимальную выпуклую оболочку</returns>
+        public static List<Point> GrahamAlgorithm(List<Point> input)
+        {
+            List<Point> points = input;
+            //Первая точка - точка с минимальной координатой Y и с минимальной координатой X при наличии совпадений
+            Point p0 = points.OrderBy(point => point.Y).ThenBy(point => point.X).First();
+            //Удаляем первую точку из последовательности для дальнейшего прохода
+            points.Remove(p0);
+            //Сортируем последовательность в порядке возрастания полярного угла относительно первой точки
+            //и при равенстве полярных углов в порядке возрастания расстояния относительно первой точки
+            points = points.OrderBy(point => p0.PolarAngle(point)).ThenBy(point => p0.Distance(point)).ToList();
+            //Удаляем точки с одинаковыми полярными углами относительно первой точки, оставляя самую дальнюю
+            points.RemoveAll(point => point != points.Last() && p0.PolarAngle(point) == p0.PolarAngle(points[points.IndexOf(point) + 1]));
+
+            if(points.Count >= 2)
+            {
+                //Создаём пустой стек
+                Stack<Point> stack = new Stack<Point>(points.Count);
+                //Добавляем первые точки
+                stack.Push(p0);
+                stack.Push(points[0]);
+                //Осуществляем проход по остальным точкам
+                for (int i = 1; i < points.Count; ++i)
+                {
+                    //Удаляем точки из стека, пока они не образуют левый поворот
+                    while(!IsLeftTurn(stack.NextToTop(), stack.Top(), points[i]))
+                    {
+                        stack.Pop();
+                    }
+                    //Добавляем текущую точку в стек для дальнейшего прохода
+                    stack.Push(points[i]);
+                }
+                //Извлекаем данные из стека, сохраняя порядок
+                int size = stack.Size();
+                List<Point> result = new List<Point>();
+                for(int i = 0; i < size; ++i)
+                {
+                    result.Add(stack.Pop());
+                }
+                result.Reverse();
+                return result;
+            }
+            //Выпуклая оболочка пуста
+            return null;
+        }
+
+        /// <summary>
+        /// Возвращает значение, образуют ли точки поворот влево
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <param name="p3"></param>
+        /// <returns></returns>
+        public static bool IsLeftTurn(Point p1, Point p2, Point p3)
+        {
+            //Если векторное произведение положительно, то точки образуют левый поворот
+            return (p2.X - p1.X) * (p3.Y - p2.Y) - (p2.Y - p1.Y) * (p3.X - p2.X) > 0;
+        }
+
+        /// <summary>
+        /// Осуществляет запись в файл в формате Plain
+        /// </summary>
+        /// <param name="path">путь к файлу</param>
+        /// <param name="points">последовательность точек</param>
+        public static void WriteFilePlain(string path, List<Point> points)
+        {
+            //Формирование текста для записи в файл
+            string result = string.Empty;
+            result += $"{points.Count}\n";
+            for(int i = 0; i < points.Count - 1; ++i)
+            {
+                result += $"{points[i].X} {points[i].Y}\n";
+            }
+            result += $"{points[points.Count - 1].X} {points[points.Count - 1].Y}";
+
+            //Запись в файл
+            try
+            {
+                File.WriteAllText(path, result);
+            }
+            catch
+            {
+                Console.WriteLine("Oops... Something wrong with writing to the file.");
+            }
+        }
+
+        /// <summary>
+        /// Осуществляет запись в файл в формате WKT
+        /// </summary>
+        /// <param name="path">путь к файлу</param>
+        /// <param name="input">последовательность точек MULTIPOINT</param>
+        /// <param name="points">последовательность точек POLYGON</param>
+        public static void WriteFileWKT(string path, List<Point> input, List<Point> points)
+        {
+            //Формирование текста для записи в файл
+            string result = string.Empty;
+            result += $"MULTIPOINT (({input[0].X} {input[0].Y}),";
+            for(int i = 1; i < input.Count - 1; ++i)
+            {
+                result += $" ({input[i].X} {input[i].Y}),";
+            }
+            result += $" ({input[input.Count - 1].X} {input[input.Count - 1].Y}))\n";
+
+            result += $"POLYGON (({points[0].X} {points[0].Y}),";
+            for (int i = 1; i < points.Count - 1; ++i)
+            {
+                result += $" ({points[i].X} {points[i].Y}),";
+            }
+            result += $" ({points[points.Count - 1].X} {points[points.Count - 1].Y}))";
+
+            //Запись в файл
+            try
+            {
+                File.WriteAllText(path, result);
+            }
+            catch
+            {
+                Console.WriteLine("Oops... Something wrong with writing to the file.");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Представляет структуру данных стек
+    /// </summary>
+    /// <typeparam name="T">тип элементов стека</typeparam>
+    class Stack<T>
+    {
+        List<T> items; //данные стека
+        public const int maxCapacity = int.MaxValue; //максимально допустимый размер стека
+
+        public Stack(int capacity = maxCapacity)
+        {
+            if (capacity > 0 && capacity <= maxCapacity)
+            {
+                items = new List<T>(capacity);
+            }
+            else throw new ArgumentException("Неверный параметр для размера стека.");
+        }
+
+        /// <summary>
+        /// Добавляет новый элемент в стек. Если стек переполнен, выбрасывает исключение StackOverflowException
+        /// </summary>
+        /// <param name="item"></param>
+        public void Push(T item)
+        {
+            if (items.Capacity != Size())
+            {
+                items.Add(item);
+            }
+            else throw new StackOverflowExceprion();
+        }
+
+        /// <summary>
+        /// Возвращает и удаляет элемент из вершины стека. Если стек пуст, выбрасывает исключение StackUnderflowException
+        /// </summary>
+        /// <returns></returns>
+        public T Pop()
+        {
+            if (!IsEmpty())
+            {
+                T item = items[Size() - 1];
+                items.RemoveAt(Size() - 1);
+                return item;
+            }
+            else throw new StackUnderflowExceprion();
+        }
+
+        /// <summary>
+        /// Возращает элемент из вершины стека. Если стек пуст, возвращает значение по умолчанию
+        /// </summary>
+        /// <returns></returns>
+        public T Top()
+        {
+            if (!IsEmpty())
+            {
+                return items[Size() - 1];
+            }
+            else return default;
+        }
+
+        /// <summary>
+        /// Возвращает следующий элемент после вершины стека. Если в стеке меньше 2 элементов, возвращает значение по умолчанию
+        /// </summary>
+        /// <returns></returns>
+        public T NextToTop()
+        {
+            if (Size() > 1)
+            {
+                return items[Size() - 2];
+            }
+            else return default;
+        }
+
+        /// <summary>
+        /// Возвращает размер стека
+        /// </summary>
+        /// <returns></returns>
+        public int Size() => items.Count;
+
+        /// <summary>
+        /// Возращает флаг, пуст ли стек
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty() => Size() == 0;
+    }
+
+    /// <summary>
+    /// Представляет точку на плоскости
+    /// </summary>
+    class Point
+    {
+        public Point(int x, int y)
+        {
+            if (x >= 0 && x <= 10000 && y >= 0 && y <= 10000)
+            {
+                X = x;
+                Y = y;
+            }
+            else throw new ArgumentException("Неверные параметры для координат точки.");
+        }
+
+        /// <summary>
+        /// Координата по оси абцисс
+        /// </summary>
+        public int X { get; }
+
+        /// <summary>
+        /// Координата по оси ординат
+        /// </summary>
+        public int Y { get; }
+
+        /// <summary>
+        /// Квадрат расстояния между точками
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int Distance(Point other) => (int)(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2));
+
+        /// <summary>
+        /// Полярный угол между точками
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public double PolarAngle(Point other) => Math.Atan2(other.Y - Y, other.X - X);
+    }
+
+    /// <summary>
+    /// Это исключение выбрасывается при переполнении стека
+    /// </summary>
+    class StackOverflowExceprion : Exception
+    {
+        public StackOverflowExceprion() : base() { }
+        public StackOverflowExceprion(string message) : base(message) { }
+        public StackOverflowExceprion(string message, Exception innerException) : base (message, innerException) { }
+        protected StackOverflowExceprion(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base (info, context) { }
+    }
+
+    /// <summary>
+    /// Это исключение выбрасывается при удалении элемента из пустого стека
+    /// </summary>
+    class StackUnderflowExceprion : Exception
+    {
+        public StackUnderflowExceprion() : base() { }
+        public StackUnderflowExceprion(string message) : base(message) { }
+        public StackUnderflowExceprion(string message, Exception innerException) : base(message, innerException) { }
+        protected StackUnderflowExceprion(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ seminars/03_hw1b_Graham_C++/solution/Program.cpp
@@ -0,0 +1,404 @@
+﻿using namespace std;
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+/// <summary>
+/// Представляет точку на плоскости
+/// </summary>
+class Point
+{
+public:
+    Point() : Point(0, 0) {}
+    Point(int x, int y)
+    {
+        if (x >= 0 && x <= 10000 && y >= 0 && y <= 10000)
+        {
+            this->x = x;
+            this->y = y;
+        }
+        else throw "Неверные параметры для координат точки.";
+    }
+    /// <summary>
+    /// Координата по оси абцисс
+    /// </summary>
+    /// <returns></returns>
+    int getX()
+    {
+        return x;
+    }
+    /// <summary>
+    /// Координата по оси ординат
+    /// </summary>
+    /// <returns></returns>
+    int getY()
+    {
+        return y;
+    }
+    /// <summary>
+    /// Квадрат расстояния между точками
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    int distance(Point& other)
+    {
+        return pow(x - other.getX(), 2) + pow(y + other.getY(), 2);
+    }
+    /// <summary>
+    /// Полярный угол между точками
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    double polarAngle(Point& other)
+    {
+        return atan2(other.getY() - y, other.getX() - x);
+    }
+
+private:
+    int x;
+    int y;
+};
+
+
+
+
+
+/// <summary>
+/// Представляет структуру данных стек
+/// </summary>
+class Stack
+{
+public:
+    static const int maxCapacity = 10000; //максимально допустимый размер стека
+    Stack(int capacity = maxCapacity)
+    {
+        if (0 < capacity && capacity <= maxCapacity)
+        {
+            this->capacity = capacity;
+        }
+        else throw "Неверный параметр для размера стека.";
+    }
+    /// <summary>
+    /// Добавляет новый элемент в стек. Если стек переполнен, выбрасывает исключение overflow_exception
+    /// </summary>
+    /// <param name="item"></param>
+    void push(Point& item)
+    {
+        if (capacity != items.size())
+        {
+            items.push_back(item);
+        }
+        else throw overflow_error("Стек переполнен.");
+    }
+    /// <summary>
+    /// Возвращает и удаляет элемент из вершины стека. Если стек пуст, выбрасывает исключение underflow_exception
+    /// </summary>
+    /// <returns></returns>
+    Point pop()
+    {
+        if (!isEmpty())
+        {
+            Point item = items[size() - 1];
+            items.pop_back();
+            return item;
+        }
+        else throw underflow_error("Стек пуст.");
+    }
+    /// <summary>
+    /// Возращает элемент из вершины стека
+    /// </summary>
+    /// <returns></returns>
+    Point top()
+    {
+        return items.back();
+    }
+    /// <summary>
+    /// Возвращает следующий элемент после вершины стека
+    /// </summary>
+    /// <returns></returns>
+    Point nextToTop()
+    {
+        Point top = pop();
+        Point next = items.back();
+        items.push_back(top);
+        return next;
+    }
+    /// <summary>
+    /// Возвращает размер стека
+    /// </summary>
+    /// <returns></returns>
+    int size()
+    {
+        return items.size();
+    }
+    /// <summary>
+    /// Возращает флаг, пуст ли стек
+    /// </summary>
+    /// <returns></returns>
+    bool isEmpty()
+    {
+        return items.empty();
+    }
+
+private:
+    vector<Point> items; //данные стека
+    int capacity; //вместимость стека
+};
+
+
+
+
+
+/// <summary>
+/// Осуществляет чтение файла
+/// </summary>
+/// <param name="path">путь к файлу</param>
+/// <returns></returns>
+static vector<Point> readFile(string path)
+{
+    vector<Point> result;
+    try
+    {
+        ifstream in;
+        in.open(path);
+        int n;
+        in >> n;
+        for (int i = 0; i < n; ++i)
+        {
+            int x, y;
+            in >> x >> y;
+            result.push_back(Point(x, y));
+        }
+        in.close();
+        return result;
+    }
+    catch (exception)
+    {
+        cout << "Ooops... Something wrong with reading the file.";
+        return vector<Point>();
+    }
+}
+
+/// <summary>
+/// Осуществляет запись в файл в формате WKT
+/// </summary>
+/// <param name="path">путь к файлу</param>
+/// <param name="input">последовательность точек MULTIPOINT</param>
+/// <param name="points">последовательность точек POLYGON</param>
+static void writeFileWKT(string path, vector<Point> input, vector<Point> points)
+{
+    //Формирование текста для записи в файл
+    string output;
+    output += "MULTIPOINT ((" + to_string(input[0].getX()) + " " + to_string(input[0].getY()) + "),";
+    for (int i = 1; i < input.size() - 1; ++i)
+    {
+        output += " (" + to_string(input[i].getX()) + " " + to_string(input[i].getY()) + "),";
+    }
+    output += " (" + to_string(input[input.size() - 1].getX()) + " " + to_string(input[input.size() - 1].getY()) + "))\n";
+
+    output += "POLYGON ((" + to_string(points[0].getX()) + " " + to_string(points[0].getY()) + "),";
+    for (int i = 1; i < points.size() - 1; ++i)
+    {
+        output += " (" + to_string(points[i].getX()) + " " + to_string(points[i].getY()) + "),";
+    }
+    output += " (" + to_string(points[points.size() - 1].getX()) + " " + to_string(points[points.size() - 1].getY()) + "))\n";
+
+    //Запись в файл
+    try
+    {
+        ofstream out(path);
+        if (out.is_open())
+        {
+            out << output;
+        }
+        out.close();
+    }
+    catch (exception)
+    {
+        cout << "Ooops.. Something wrong with writing the file.";
+        return;
+    }
+}
+
+/// <summary>
+/// Осуществляет запись в файл в формате Plain
+/// </summary>
+/// <param name="path">путь к файлу</param>
+/// <param name="points">последовательность точек</param>
+static void writeFilePlain(string path, vector<Point> points)
+{
+    //Формирование текста для записи в файл
+    string output;
+    output += to_string(points.size()) + "\n";
+    for (int i = 0; i < points.size() - 1; ++i)
+    {
+        output += to_string(points[i].getX()) + " " + to_string(points[i].getY()) + "\n";
+    }
+    output += to_string(points[points.size() - 1].getX()) + " " + to_string(points[points.size() - 1].getY());
+
+    //Запись в файл
+    try
+    {
+        ofstream out;
+        out.open(path);
+        out << output;
+        out.close();
+    }
+    catch (exception)
+    {
+        cout << "Ooops.. Something wrong with writing the file.";
+        return;
+    }
+}
+
+
+
+
+
+/// <summary>
+/// Возвращает значение, образуют ли точки поворот влево
+/// </summary>
+/// <param name="p1"></param>
+/// <param name="p2"></param>
+/// <param name="p3"></param>
+/// <returns></returns>
+static bool isLeftTurn(Point p1, Point p2, Point p3)
+{
+    //Если векторное произведение положительно, то точки образуют левый поворот
+    return (p2.getX() - p1.getX()) * (p3.getY() - p2.getY()) - (p2.getY() - p1.getY()) * (p3.getX() - p2.getX()) > 0;
+}
+
+/// <summary>
+/// Компоратор для сортировки сначала по осе ординат, потом по оси абцисс
+/// </summary>
+/// <param name="p1"></param>
+/// <param name="p2"></param>
+/// <returns></returns>
+static bool cmp1(Point p1, Point p2)
+{
+    return p1.getY() < p2.getY() || p1.getY() == p2.getY() && p1.getX() < p2.getX();
+}
+
+/// <summary>
+/// Компоратор для сортировки сначала по полярному углу с точкой p0, потом по дистанции относительно точки p0
+/// </summary>
+/// <param name="p0"></param>
+/// <param name="p1"></param>
+/// <param name="p2"></param>
+/// <returns></returns>
+static bool cmp2(Point p0, Point p1, Point p2)
+{
+    return p0.polarAngle(p1) < p0.polarAngle(p2) || p0.polarAngle(p1) == p0.polarAngle(p2) && p0.distance(p1) < p0.distance(p2);
+}
+
+/// <summary>
+/// Осуществляет поиск минимальной выпуклой оболочки с помощью прохода Грэхема на основе стека
+/// Проход осуществляется против часовой стрелки
+/// </summary>
+/// <param name="input">исходная последовательность точек</param>
+/// <returns>последовательность точек, которые образуют минимальную выпуклую оболочку</returns>
+static vector<Point> GrahamAlgorithm(vector<Point> input)
+{
+    vector<Point> points = input;
+    //Первая точка - точка с минимальной координатой Y и с минимальной координатой X при наличии совпадений
+    sort(points.begin(), points.end(), cmp1);
+    Point p0 = points[0];
+    //Удаляем первую точку из последовательности для дальнейшего прохода
+    points.erase(points.cbegin());
+    //Сортируем последовательность в порядке возрастания полярного угла относительно первой точки
+    //и при равенстве полярных углов в порядке возрастания расстояния относительно первой точки
+    for (int i = 0; i < points.size() - 1; ++i)
+    {
+        for (int j = 0; j < points.size() - 1; ++j)
+        {
+            if (!(cmp2(p0, points[j], points[j + 1])))
+            {
+                Point temp = points[j];
+                points[j] = points[j + 1];
+                points[j + 1] = temp;
+            }
+        }
+    }
+    //Удаляем точки с одинаковыми полярными углами относительно первой точки, оставляя самую дальнюю
+    for (auto it = points.begin(); it != points.end() - 1;)
+    {
+        if (p0.polarAngle(*it) == p0.polarAngle(*(it + 1)))
+        {
+            it = points.erase(it);
+        }
+        else ++it;
+    }
+    if (points.size() >= 2)
+    {
+        //Создаём пустой стек
+        Stack stack = Stack(points.size());
+        //Добавляем первые точки
+        stack.push(p0);
+        stack.push(points[0]);
+        //Осуществляем проход по остальным точкам
+        for (int i = 1; i < points.size(); ++i)
+        {
+            //Удаляем точки из стека, пока они не образуют левый поворот
+            while (!isLeftTurn(stack.nextToTop(), stack.top(), points[i]))
+            {
+                stack.pop();
+            }
+            //Добавляем текущую точку в стек для дальнейшего прохода
+            stack.push(points[i]);
+        }
+        //Извлекаем данные из стека, сохраняя порядок
+        int size = stack.size();
+        vector<Point> result;
+        for (int i = 0; i < size; ++i)
+        {
+            result.push_back(stack.pop());
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+    else
+    {
+        cout << "MBO is empty.";
+        return vector<Point>();
+    }
+}
+
+
+
+
+
+int main(int argc, char **argv)
+{
+    //Чтение файла
+    vector<Point> points = readFile((string)(argv[3]));
+    if (!points.empty())
+    {
+        //Осуществление прохода по точкам
+        vector<Point> mbo = GrahamAlgorithm(points);
+        if (!mbo.empty())
+        {
+            //Добавляем первую точку в конец стека для удобных преобразований при выводе без потери первой точки
+            mbo.push_back(mbo[0]);
+            //Если нужен проход по часовой стрелке, то переворачиваем последовательность
+            if ((string)(argv[1]) == "cw")
+            {
+                reverse(mbo.begin(), mbo.end());
+            }
+            //Выбор соответствующего формата записи данных в файл
+            if ((string)(argv[2]) == "plain")
+            {
+                mbo.pop_back();
+                writeFilePlain((string)(argv[4]), mbo);
+            }
+            else
+            {
+                writeFileWKT((string)(argv[4]), points, mbo);
+            }
+        }
+    }
+    return 0;
+}
