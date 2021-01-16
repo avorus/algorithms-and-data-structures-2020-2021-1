eac87f4a8de02b17e98de3db3b184a4644866ae8
Churt's <64949882+Crutchs@users.noreply.github.com>
2021-01-16 16:17:31 +0300

    add seminars part 2

---
+++ seminars/04_cw_Bitmap_C#/solution/Program.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Bitmaps
+{
+    class Program
+    {
+        /// <summary>
+        /// Путь к каталогу с файлами таблиц данных
+        /// </summary>
+        static string dataPath;
+        /// <summary>
+        /// Путь к файлу с входными данными
+        /// </summary>
+        static string inputPath;
+        /// <summary>
+        /// Путь к файлу с выходными данными
+        /// </summary>
+        static string outputPath;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Чтение аргументов командной строки
+                dataPath = args[0];
+                inputPath = args[1];
+                outputPath = args[2];
+
+                InvisibleJoin();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"При выполнении программы возникло следующее исключение: {ex.Message}");
+            }
+        }
+
+        //Поля таблицы DimProduct
+        static string[] product = {"ProductKey", "ProductAlternateKey", "EnglishProductName", "Color", "SafetyStockLevel", "ReorderPoint",
+                    "SizeRange", "DaysToManufacture", "StartDate"};
+        //Поля таблицы DimReseller
+        static string[] reseller = {"ResellerKey", "ResellerAlternateKey", "Phone", "BusinessType", "ResellerName",
+                    "NumberEmployees", "OrderFrequency", "ProductLine", "AddressLinel", "BankName", "YearOpened"};
+        //Поля таблицы DimCurrency
+        static string[] currency = { "CurrencyKey", "CurrencyAlternateKey", "CurrencyName" };
+        //Поля таблицы DimPromotion
+        static string[] promotion = {"PromotionKey", "PromotionAlternateKey", "EnglishPromotionName",
+                    "EnglishPromotionType", "EnglishPromotionCategory", "StartDate", "EndDate", "MinQty"};
+        //Поля таблицы DimSalesTerritory
+        static string[] sales = {"SalesTerritoryKey", "SalesTerritoryAlternateKey", "SalesTerritoryRegion",
+                    "SalesTerritoryCountry", "SalesTerritoryGroup"};
+        //Поля таблицы DimEmployee
+        static string[] employee = {"EmployeeKey", "FirstName", "LastName", "Title", "BirthDate", "LoginID",
+                    "EmailAddress", "Phone", "MaritalStatus", "Gender", "PayFrequency", "VacationHours",
+                    "SickLeaveHours", "DepartmentName", "StartDate"};
+        //Поля таблицы DimDate
+        static string[] date = {"DateKey", "FullDateAlternateKey", "DayNumberOfWeek", "EnglishDayNameOfWeek",
+                    "DayNumberOfMonth", "DayNumberOfYear", "WeekNumberOfYear", "EnglishMonthName", "MonthNumberOfYear",
+                    "CalendarQuarter", "CalendarYear", "CalendarSemester", "FiscalQuarter", "FiscalYear",
+                    "FiscalSemester"};
+
+        public static void InvisibleJoin()
+        {
+            List<string> outFields; //список полей таблиц, которые соответствуют выходным данным
+            List<string> filters = new List<string>(); //список фильтров в формате {<table>.<field> <operation> <value>}
+
+            //Чтение входных данных
+            using (StreamReader sr = new StreamReader(inputPath))
+            {
+                outFields = new List<string>(sr.ReadLine().Split(','));
+                int amountFilters = int.Parse(sr.ReadLine());
+                for (int i = 0; i < amountFilters; ++i)
+                {
+                    filters.Add(sr.ReadLine());
+                }
+            }
+
+            BitArray bitmap = null; //общий битмап, равный побитовой конъюкции остальных
+            foreach (var filter in filters)
+            {
+                //Обработка фильтра
+                string[] filt = SplitFilter(filter); //{table.field, operation, value}
+                int index = filt[0].IndexOf('.');
+                string tableName = filt[0].Substring(0, index);
+                string fieldName = filt[0].Substring(index + 1);
+                BitArray current = null; //текущий bitmap
+                //Получение bitmap в зависимости от таблицы (фаза 1 и часть фазы 2 алгоритма)
+                switch (tableName)
+                {
+                    case "DimProduct":
+                        current = FilterDimTable(tableName, Array.IndexOf(product, fieldName), product[0], filt);
+                        break;
+                    case "DimReseller":
+                        current = FilterDimTable(tableName, Array.IndexOf(reseller, fieldName), reseller[0], filt);
+                        break;
+                    case "DimCurrency":
+                        current = FilterDimTable(tableName, Array.IndexOf(currency, fieldName), currency[0], filt);
+                        break;
+                    case "DimPromotion":
+                        current = FilterDimTable(tableName, Array.IndexOf(promotion, fieldName), promotion[0], filt);
+                        break;
+                    case "DimSalesTerritory":
+                        current = FilterDimTable(tableName, Array.IndexOf(sales, fieldName), sales[0], filt);
+                        break;
+                    case "DimEmployee":
+                        current = FilterDimTable(tableName, Array.IndexOf(employee, fieldName), employee[0], filt);
+                        break;
+                    case "DimDate":
+                        current = FilterDimTable(tableName, Array.IndexOf(date, fieldName), "OrderDateKey", filt);
+                        break;
+                    case "FactResellerSales":
+                        current = FilterFactTable(filt[0], filt);
+                        break;
+                }
+                //Конъюнкция bitmaps (часть фазы 2 алгоритма)
+                if (bitmap == null)
+                {
+                    bitmap = current;
+                }
+                else
+                {
+                    bitmap.And(current);
+                }
+            }
+
+            //Вывод данных в файл
+            File.WriteAllText(outputPath, GetOutput(outFields, bitmap));
+        }
+
+        /// <summary>
+        /// Обрабатывает фильтр таблицы, представляя его в виде массива значимых частей.
+        /// </summary>
+        /// <param name="filter">исходный фильтр</param>
+        /// <returns>фильтр в формате {tableName.fieldName, operation, value}</returns>
+        public static string[] SplitFilter(string filter)
+        {
+            //Примечание. Метод Split из стандартной библиотеки не подходит, так как в value может быть пробел. 
+            string[] result = new string[3];
+            int index1 = filter.IndexOf(" ");
+            int index2 = filter.Substring(index1 + 1).IndexOf(" ") + index1 + 1;
+            result[0] = filter.Substring(0, index1); //tableName.fieldName
+            result[1] = filter.Substring(index1 + 1, index2 - index1 - 1); //operation
+            result[2] = filter.Substring(index2 + 1); //value
+            return result;
+        }
+
+        /// <summary>
+        /// Применяет итоговый bitmap к таблице фактов (столбцу), получая значений полей выходных данных для вывода.
+        /// </summary>
+        /// <param name="tableName">имя таблицы фактов (столбца)</param>
+        /// <param name="bitmap"></param>
+        /// <param name="output"></param>
+        public static string GetOutput(List<string> tablesNames, BitArray bitmap)
+        {
+            bool flag = (bitmap == null); //если bitmap пустой, то нет фильтрации
+            StringBuilder[] outputs = flag
+                ? new StringBuilder[File.ReadAllLines(Path.Combine(dataPath, $"{tablesNames[0]}.csv")).Count()]
+                : new StringBuilder[bitmap.Capacity];
+            foreach (var tableName in tablesNames)
+            {
+                int index = 0;
+                int counter = 0;
+                using (StreamReader sr = new StreamReader(Path.Combine(dataPath, $"{tableName}.csv")))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        if (flag || bitmap.Get(index++))
+                        {
+                            if (outputs[counter] == null)
+                            {
+                                outputs[counter++] = new StringBuilder($"{line}");
+                            }
+                            else
+                            {
+                                outputs[counter++].Append($"|{line}");
+                            }
+                        }
+                    }
+                }
+            }
+            string output = (outputs.Count(line => line != null) > 0) //если в массиве есть значения
+                ? String.Join("\n", outputs.Where(line => line != null).Select(line => line.ToString()).ToArray()) + "\n"
+                : "\n";
+            return output;
+        }
+
+        /// <summary>
+        /// Фильтрует таблицу измерений и применяет полученные ключи к таблице фактов, 
+        /// возвращая полученный на фазе 2 агоритма bitmap.
+        /// </summary>
+        /// <param name="tableName">имя соответствующей таблицы измерений</param>
+        /// <param name="fieldIndex">индекс поля в таблице измерений, по которому идёт фильтрация</param>
+        /// <param name="key">ключ таблицы измерений</param>
+        /// <param name="filter">фильтр</param>
+        /// <returns></returns>
+        public static BitArray FilterDimTable(string tableName, int fieldIndex, string key, string[] filter)
+        {
+            //Фаза 1 алгоритма
+            HashSet<uint> keys = new HashSet<uint>(); //множество ключей, полученных на фазе 1 алгоритма
+            BitArray bitmap;
+            using (StreamReader sr = new StreamReader(Path.Combine(dataPath, $"{tableName}.csv")))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split('|');
+                    if (Operation(line[fieldIndex], filter[2], filter[1]))
+                    {
+                        keys.Add(uint.Parse(line[0]));
+                    }
+                }
+            }
+
+            //Часть фазы 2 алгоритма
+            int size = File.ReadAllLines(Path.Combine(dataPath, $"FactResellerSales.{key}.csv")).Length;
+            int index = 0;
+            bitmap = new BitArray(size);
+            using (StreamReader sr = new StreamReader(Path.Combine(dataPath, $"FactResellerSales.{key}.csv")))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    bitmap.Set(index++, keys.Contains(uint.Parse(line)));
+                }
+            }
+            return bitmap;
+        }
+
+        /// <summary>
+        /// Фильтрует таблицу фактов, возвращая полученный на фазе 2 агоритма bitmap.
+        /// </summary>
+        /// <param name="tableName">имя соответствующей таблицы фактов</param>
+        /// <param name="filter">фильтр</param>
+        /// <returns></returns>
+        public static BitArray FilterFactTable(string tableName, string[] filter)
+        {
+            BitArray bitmap;
+            int size = File.ReadAllLines(Path.Combine(dataPath, $"{tableName}.csv")).Length;
+            int index = 0;
+            bitmap = new BitArray(size);
+            using (StreamReader sr = new StreamReader(Path.Combine(dataPath, $"{tableName}.csv")))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    bitmap.Set(index++, Operation(line, filter[2], filter[1]));
+                }
+            }
+            return bitmap;
+        }
+
+        /// <summary>
+        /// Обрабатывает операцию сравнения и применяет её к данным целочисленным или строковым значениям.
+        /// В случае некорректной операции или некорректного значения выбрасывает исключение.
+        /// </summary>
+        /// <param name="a">значение слева</param>
+        /// <param name="b">значение справа</param>
+        /// <param name="op">оператор</param>
+        /// <returns>результат операции</returns>
+        public static bool Operation(string a, string b, string op)
+        {
+            //Примечание. Представлено две реализации:
+            //1) Если мы не выделяем строки с помощью '<string>'
+            /*int x, y;
+                    if (int.TryParse(a, out x) && int.TryParse(b, out y))
+            {
+                return OperationInt(x, y, op);
+            }
+            else
+            {
+                return OperationString(a, b, op);
+            }
+            */
+            //2) Если строковые значения выделяются с помощью '<string>'
+            if (b.Contains('\''))
+            {
+                return OperationString(a, b.Substring(1, b.Length - 2), op);
+            }
+            return OperationInt(int.Parse(a), int.Parse(b), op);
+        }
+
+        /// <summary>
+        /// Обрабатывает операцию сравнения и применяет её к данным строковым значениям.
+        /// В случае некорректной операции выбрасывает исключение.
+        /// </summary>
+        /// <param name="a">значение слева</param>
+        /// <param name="b">значение справа</param>
+        /// <param name="op">оператор</param>
+        /// <returns>результат операции</returns>
+        public static bool OperationString(string a, string b, string op)
+        {
+            switch (op)
+            {
+                case "=": return a.Equals(b);
+                case "<>": return !a.Equals(b);
+                default: throw new ArgumentException("Некорректная операция для строк.");
+            }
+        }
+
+        /// <summary>
+        /// Обрабатывает операцию сравнения и применяет её к данным целочисленным значениям.
+        /// В случае некорректной операции выбрасывает исключение.
+        /// </summary>
+        /// <param name="a">значение слева</param>
+        /// <param name="b">значение справа</param>
+        /// <param name="op">оператор</param>
+        /// <returns>результат операции</returns>
+        public static bool OperationInt(int a, int b, string op)
+        {
+            switch (op)
+            {
+                case "<": return a < b;
+                case ">": return a > b;
+                case "<=": return a <= b;
+                case ">=": return a >= b;
+                case "=": return a == b;
+                case "<>": return a != b;
+                default: throw new ArgumentException("Некорректная операция для целых чисел.");
+            }
+        }
+    }
+
+
+
+
+
+    /// <summary>
+    /// Абстрактный класс, представляющий структуру данных "Битовая карта".
+    /// </summary>
+    abstract class Bitmap
+    {
+        /// <summary>
+        /// Выполняет операцию логического И текущего Bitmap с другим, записывая результат в вызываемый объект.
+        /// </summary>
+        /// <param name="other"></param>
+        public abstract void And(Bitmap other);
+        /// <summary>
+        /// Устанавливает значение бита по определенному индексу.
+        /// </summary>
+        /// <param name="i">индекс бита</param>
+        /// <param name="value">устанавливаемое значение</param>
+        public abstract void Set(int i, bool value);
+        /// <summary>
+        /// Возвращает значение бита по определенному индексу.
+        /// </summary>
+        /// <param name="i">индекс</param>
+        /// <returns>значение данного бита</returns>
+        public abstract bool Get(int i);
+    }
+
+    /// <summary>
+    /// Класс, представляющий структуру данных "Битовая карта" с помощью массива 32-ух битных значений.
+    /// </summary>
+    class BitArray : Bitmap
+    {
+        private uint[] data; //данные
+        public int Capacity { get; private set; } //вместимость СД
+        public BitArray(int capacity)
+        {
+            data = new uint[capacity / 32 + 1];
+            Capacity = capacity;
+        }
+
+        public override void And(Bitmap other)
+        {
+            if (other is BitArray) //реализация для 32-ух битных значений
+            {
+                for (int i = 0; i < data.Length; ++i)
+                {
+                    data[i] = data[i] & (other as BitArray).Get32(i);
+                }
+            }
+            else //общая реализация
+            {
+                for (int i = 0; i < data.Length; ++i)
+                {
+                    for (int j = 0; j < 32; ++j)
+                    {
+                        int index = (i << 5) + j; //(i << 5) == 32 * i
+                        Set(index, Get(index) & other.Get(index));
+                    }
+                }
+            }
+        }
+
+        public override void Set(int i, bool value)
+        {
+            //Примечание. Предложено две реализации:
+            //1) Если данный бит не равен устанавливаемому значению, то его необходимо инвертировать.
+            if (Get(i) != value)
+            {
+                data[i / 32] = data[i / 32] ^ (uint)(1 << (i % 32));
+            }
+            //2) Прямая установка значения бита без проверки.
+            /*if(value)
+            {
+                data[i / 32] = data[i / 32] | (uint)(1 << (i % 32));
+            }
+            else
+            {
+                data[i / 32] = data[i / 32] & (~((uint)(1 << (i % 32))));
+            }*/
+        }
+
+        public override bool Get(int i)
+        {
+            uint temp = (uint)(1 << (i % 32));
+            return (data[i / 32] & temp) == temp;
+        }
+
+        private uint Get32(int i) => data[i]; //для 32-ух битных значений; возвращает все 32 бита
+    }
+}
---
+++ seminars/06_hw4_BTree_C++/solution/Program.cpp
@@ -0,0 +1,249 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <exception>
+
+using namespace std;
+
+/// <summary>
+/// Структура узла B-дерева
+/// </summary>
+struct Node {
+    int count; //количество ключей
+    vector<pair<int, int>> keys; //пары ключ-значение
+    bool leaf; //является ли узел листом
+    vector<Node*> children; //дети
+};
+
+/// <summary>
+/// Класс B-дерева
+/// </summary>
+class Tree {
+private:
+    Node* root; //корень
+    int t; //минимальная степень ветвления
+
+    /// <summary>
+    /// Поиск значения по ключу в узле В-дерева
+    /// </summary>
+    /// <param name="node">узел</param>
+    /// <param name="key">ключ</param>
+    /// <returns>пару значений: флаг, есть ли такой ключ, и значение по данному ключу</returns>
+    pair<bool, int> searchNode(Node* node, int key) {
+        if (node != nullptr)
+        {
+            int i = 0;
+            while ((i < node->count) && (key > node->keys[i].first)) {
+                i++;
+            }
+            if ((i < node->count) && (key == node->keys[i].first)) {
+                return pair<bool, int>(true, node->keys[i].second);
+            }
+            else if (node->leaf) {
+                return pair<bool, int>(false, NULL);
+            }
+            else {
+                return searchNode(node->children[i], key);
+            }
+        }
+        return pair<bool, int>(false, NULL);
+    }
+
+    /// <summary>
+    /// Разбиение узла В-дерева
+    /// </summary>
+    /// <param name="node">родительский узел ребёнка, который подлежит разбиению</param>
+    /// <param name="index">индекс нового ключа в данном узле</param>
+    void splitNode(Node* node, int index) {
+        Node* childNode = node->children[index];
+        Node* newNode = new Node();
+        newNode->leaf = childNode->leaf;
+        newNode->count = t - 1;
+        for (int i = 0; i < t - 1; ++i) {
+            newNode->keys.push_back(childNode->keys[i + t]);
+        }
+        if (!childNode->leaf) {
+            for (int i = 0; i < t; ++i) {
+                newNode->children.push_back(childNode->children[i + t]);
+            }
+            for (int i = 0; i < t; ++i) {
+                childNode->children.pop_back();
+            }
+        }
+        childNode->count = t - 1;
+        node->children.push_back(nullptr);
+        for (int i = node->count; i >= index + 1; --i) {
+            node->children[i + 1] = node->children[i];
+        }
+        node->children[index + 1] = newNode;
+        node->keys.emplace_back(0, 0);
+        for (int i = node->count - 1; i >= index; --i) {
+            node->keys[i + 1] = node->keys[i];
+        }
+        node->keys[index] = childNode->keys[t - 1];
+        node->count++;
+    }
+
+    /// <summary>
+    /// Добавление ключа с значением в узел В-дерева
+    /// </summary>
+    /// <param name="node">узел</param>
+    /// <param name="key">ключ</param>
+    /// <param name="value">значение, связанное с данным ключом (payload)</param>
+    void insertNode(Node* node, int key, int value) {
+        int i = node->count - 1;
+        if (node->leaf) {
+            node->keys.emplace_back(0, 0);
+            while ((i >= 0) && (key < node->keys[i].first)) {
+                node->keys[i + 1] = node->keys[i];
+                i--;
+            }
+            node->keys[i + 1] = pair<int, int>(key, value);
+            node->count++;
+        }
+        else {
+            while ((i >= 0) && (key < node->keys[i].first)) {
+                i--;
+            }
+            i++;
+            Node* child = node->children[i];
+            if (child->count == 2 * t - 1) {
+                splitNode(node, i);
+                if (key > node->keys[i].first) {
+                    i++;
+                }
+            }
+            insertNode(node->children[i], key, value);
+        }
+    }
+
+    /// <summary>
+    /// Очистка узла дерева
+    /// </summary>
+    /// <param name="node">узел</param>
+    void deleteNode(Node* node) {
+        if (node != nullptr) {
+            for (int i = 0; i < node->count + 1; ++i) {
+                deleteNode(node->children[i]);
+            }
+            delete (node);
+        }
+    }
+
+public:
+    /// <summary>
+    /// Конструктор нового B-дерева
+    /// </summary>
+    /// <param name="degree">минимальная степень ветвления</param>
+    explicit Tree(int degree) {
+        root = nullptr;
+        t = degree;
+    }
+
+    /// <summary>
+    /// Поиск значения по ключу
+    /// </summary>
+    /// <param name="key">ключ</param>
+    /// <returns>пару значений: флаг, есть ли такой ключ, и значение по данному ключу</returns>
+    pair<bool, int> search(int key) {
+        return searchNode(root, key);
+    }
+
+    /// <summary>
+    /// Добавление ключа с значением
+    /// </summary>
+    /// <param name="key">ключ</param>
+    /// <param name="value">значение, связанное с данным ключом (payload)</param>
+    /// <returns>флаг, успешно ли добавление ключа</returns>
+    bool insert(int key, int value) {
+        if (root != nullptr)
+        {
+            if (!search(key).first) {
+                if (root->count == 2 * t - 1) {
+                    Node* oldRoot = root;
+                    Node* newRoot = new Node();
+                    root = newRoot;
+                    newRoot->leaf = false;
+                    newRoot->count = 0;
+                    newRoot->children.push_back(oldRoot);
+                    splitNode(newRoot, 0);
+                    insertNode(newRoot, key, value);
+                }
+                else {
+                    insertNode(root, key, value);
+                }
+                return true;
+            }
+            return false;
+        }
+        //создание корня для пустого дерева
+        root = new Node();
+        root->keys.emplace_back(key, value);
+        root->count = 1;
+        root->leaf = true;
+        return true;
+    }
+
+    ~Tree() {
+        deleteNode(root);
+    }
+};
+
+int main(int argc, char** argv) {
+    int t = 1; //минимальная степень ветвления
+    string ipath; //путь к файлу с входными данными
+    string opath; //путь к файлу с выходными данными
+    try
+    {
+        t = stoi(argv[1]);
+        if (t <= 1)
+        {
+            cout << "Ooops.. Something wrong with arguments ." << endl;
+            cout << "The minimum number of keys must be a natural number." << endl;
+            return 0;
+        }
+        ipath = argv[2];
+        opath = argv[3];
+    }
+    catch (exception ex)
+    {
+        cout << "Ooops.. Something wrong with arguments ." << endl;
+        cout << ex.what() << endl;
+        return 0;
+    }
+
+    Tree* tree = new Tree(t); //В-дерево
+    try
+    {
+        ifstream in; //поток для чтения
+        ofstream out; //поток для записи
+        in.open(ipath);
+        out.open(opath);
+        string command; //ввод команд "insert" или "find"
+        string key, value; //ввод ключа и значения
+        pair<bool, int> result; //результат работы поиска значения по ключу
+        while (in >> command >> key)
+        {
+            if (command == "insert")
+            {
+                in >> value;
+                out << (tree->insert(stoi(key), stoi(value)) ? "true" : "false") << endl;
+            }
+            else if (command == "find")
+            {
+                result = tree->search(stoi(key));
+                out << (result.first ? to_string(result.second) : "null") << endl;
+            }
+        }
+        in.close();
+        out.close();
+    }
+    catch (exception ex)
+    {
+        cout << "Ooops... Something wrong with reading and writing the file." << endl;
+        cout << ex.what() << endl;
+        return 0;
+    }
+    return 0;
+}
